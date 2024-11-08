3fa49d51162943e576a9637c301ac062a317a0dc
hasibimamhridoy <hasibimamhridoy@gmail.com>
2024-11-08 07:06:16 +0600

    fix email

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -27,7 +27,7 @@ export const sendEmail = async (formData: FormData) => {
   try {
     data = await resend.emails.send({
       from: "onboarding@resend.dev",
-      to: process.env.RESEND_TO_EMAIL as string,
+      to: "hasibimamhridoy@gmail.com",
       subject: "Message from contact form",
       reply_to: senderEmail,
       react: React.createElement(ContactFormEmail, {
